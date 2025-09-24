316331855f59dbb4abf51fa165221683ac377c7e
nicolas <nicolas.s.guim67@gmail.com>
2025-09-24 10:06:42 -0300

    Acesso a Project 2 e com integração a api de notificação

--- templates/service-worker.js
+++ templates/service-worker.js
@@ -1,1 +1,14 @@
 importScript('https://js.pusher.com/beams/service-worker.js');
+<script src="https://js.pusher.com/beams/2.1.0/push-notifications-cdn.js"></script> script do SKD
+
+ <script>
+  const beamsClient = new PusherPushNotifications.Client({
+    instanceId: 'ad961b7a-2772-4626-bf14-2cd23de153a7',
+  });
+
+  beamsClient.start()
+    .then(() => beamsClient.addDeviceInterest('hello'))
+    .then(() => console.log('Successfully registered and subscribed!'))
+    .catch(console.error);
+    
+</script>   
